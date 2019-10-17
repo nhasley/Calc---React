6efe7c2dee6db448e4b48be86236afc87451f8db
Nova <novahasley@gmail.com>
2019-10-17 14:23:13 -0500

    added rows

--- src/App.js
+++ src/App.js
@@ -9,6 +9,27 @@ class App extends Component {
         <div className='calc-wrapper'>
           <div className="row">
             <Button>7</Button>
+            <Button>8</Button>
+            <Button>9</Button>
+            <Button>/</Button>
+          </div>
+          <div className="row">
+            <Button>4</Button>
+            <Button>5</Button>
+            <Button>6</Button>
+            <Button>X</Button>
+          </div>
+          <div className="row">
+            <Button>1</Button>
+            <Button>2</Button>
+            <Button>3</Button>
+            <Button>+</Button>
+          </div>
+          <div className="row">
+            <Button>.</Button>
+            <Button>0</Button>
+            <Button>=</Button>
+            <Button>-</Button>
           </div>
         </div>
       </div>
